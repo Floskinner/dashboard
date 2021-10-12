acd4693715d64b8ac3e09cfa39bc75432a6ded63
Florian Glaser <58706771+Floskinner@users.noreply.github.com>
2021-10-12 17:04:27 +0200

    [feat]Ping Tests: Added Pylint-Tests for the uptimer_service.py (#3)
    * added some comments
    * added pytest for Ping
    * some pylint fixes

---
+++ tests/test_uptimer_service.py
@@ -0,0 +1,59 @@
+import pytest
+from models.service import DBService, PingService
+from models.service_error import ServiceBulkException
+from pytest_httpx import HTTPXMock
+from pytest_mock import MockerFixture
+from services import uptimer_service
+
+
+@pytest.fixture
+def db_service() -> DBService:
+    data = {"name": "test", "url": "https://test.url", "ping": False}
+    return DBService(**data)
+
+
+@pytest.mark.parametrize(
+    "number_of_requests",
+    [
+        (1),
+        (10),
+    ],
+)
+@pytest.mark.asyncio
+async def test_ping_ok(mocker: MockerFixture, httpx_mock: HTTPXMock, number_of_requests: int, db_service: DBService):
+    httpx_mock.add_response(status_code=200, url=db_service.url)
+    mocker.patch("services.uptimer_service.get_service", return_value=db_service)
+
+    requested_services = [db_service for i in range(number_of_requests)]
+    excpected_response = [
+        PingService(**db_service.get_attributes(), response_time=0) for i in range(number_of_requests)
+    ]
+
+    response = await uptimer_service.ping_service(requested_services)
+    assert response == excpected_response
+
+
+@pytest.mark.parametrize(
+    "number_of_requests",
+    [
+        (1),
+        (10),
+    ],
+)
+@pytest.mark.asyncio
+async def test_ping_fail(mocker: MockerFixture, httpx_mock: HTTPXMock, number_of_requests: int, db_service: DBService):
+    requested_services = [db_service for i in range(number_of_requests)]
+
+    # Raise because no service Found
+    with pytest.raises(ServiceBulkException):
+        await uptimer_service.ping_service(requested_services)
+
+    # Raise because Service can not be resolved
+    mocker.patch("services.uptimer_service.get_service", return_value=db_service)
+    with pytest.raises(ServiceBulkException):
+        await uptimer_service.ping_service(requested_services)
+
+    # Raise because Service got no 2XX status code
+    httpx_mock.add_response(status_code=404, url=db_service.url)
+    with pytest.raises(ServiceBulkException):
+        await uptimer_service.ping_service(requested_services)
